23c446241c7ad73ebb144cfe735cdc13eb9719bd
mnq11 <mnq_77@yahoo.com>
2023-04-23 01:52:34 +0200

    start

--- src/api/error.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    res.status(400).json({ error: 'Error message' });
-};
-
-export default handler;
