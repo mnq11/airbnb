26d0d798b981e2896b7c1fd89bbc831b3e3d11b9
mnq11 <mnq_77@yahoo.com>
2023-04-26 09:08:35 +0200

    working on the view counter add vie to db

--- app/components/ViewCounter.tsx
+++ app/components/ViewCounter.tsx
@@ -2,7 +2,7 @@
 
 import {SafeUser} from "@/app/types";
 import React from "react";
-import {BsEye, BsEyeFill} from "react-icons/all";
+import { BsEye, BsEyeFill } from 'react-icons/bs';
 
 interface viewCounterProps {
     listingId: string
@@ -15,38 +15,28 @@ const ViewCounter: React.FC<viewCounterProps> = ({
                                                      currentUser,
                                                      viewCounter,
                                                  }) => {
-
-
     return (
-        <div
-            className="relative hover:opacity-80 transition cursor-pointer z-10"
-        >
-            <div className="flex flex-col items-left">
-                <BsEye
-                    size={28}
-                    className="fill-white absolute -top-[2px] -right-[2px]"
-                />
-                <BsEyeFill
-                    size={24}
-                    className={viewCounter ? "fill-rose-500" : "fill-neutral-500/70"}
-                />
-                {/* Update the viewCounter display */}
-                <div className="font-extrabold text-rose-500 text-center mt-1 select-none pointer-events-none">
+        <div className="relative hover:opacity-80 transition cursor-pointer z-10">
+            <div className="flex flex-row items-center">
+                <div className="font-extrabold text-red-700 mr-9 select-none pointer-events-none">
                     {`${(viewCounter || 0).toLocaleString()}`}
                 </div>
-
-                <style jsx>{`
-                  .select-none {
-                    user-select: none;
-                  }
-
-                  .pointer-events-none {
-                    pointer-events: none;
-                  }
-                `}</style>
+                <BsEye
+                    size={28}
+                    className={viewCounter ? "text-red-700" : "text-neutral-500/70"}
+                />
             </div>
+            <style jsx>{`
+      .select-none {
+        user-select: none;
+      }
+
+      .pointer-events-none {
+        pointer-events: none;
+      }
+    `}</style>
         </div>
     );
 };
 
-export default ViewCounter;
+    export default ViewCounter;
