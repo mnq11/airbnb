5a33a6ebacd04006fa810c07495176fa11b6fdb9
mnq11 <mnq_77@yahoo.com>
2023-04-28 10:25:59 +0200

    some changes to the CategoryBox.tsx slider

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -43,30 +43,31 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
     router.push(url);
   }, [label, router, params]);
 
-  return ( 
-    <div
-      onClick={handleClick}
-      className={`
+  return (
+      <div
+          onClick={handleClick}
+          className={`
         flex 
         flex-col 
         items-center 
         justify-center 
         gap-2
         p-3
-        border-b-2
-        hover:text-neutral-800
+        border-b-4
+        rounded
+        hover:bg-blue-100
         transition
         cursor-pointer
-        ${selected ? 'border-b-neutral-800' : 'border-transparent'}
+        ${selected ? 'border-blue-600' : 'border-transparent'}
         ${selected ? 'text-neutral-800' : 'text-neutral-500'}
       `}
-    >
-      <Icon size={26} />
-      <div className="font-medium text-sm">
-        {label}
+      >
+        <Icon size={26} />
+        <div className="font-medium text-sm">
+          {label}
+        </div>
       </div>
-    </div>
-   );
+  );
 }
  
 export default CategoryBox;
