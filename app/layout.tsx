3c332f77af41cb63cc63de07a4493804bfeb3ab8
mnq11 <mnq_77@yahoo.com>
2023-04-29 11:28:41 +0200

    link metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ import getCurrentUser from './actions/getCurrentUser';
 
 export const metadata = {
   title: 'جولتنا',
-  description: 'منصة جولتنا هي منصة تساعد المستخدمة لتحديد المناطق الذي نعتقد انها تستحق قضاء جولة فيها بأرخص الاسعار',
+  description: 'منصة جولتنا للتجوال',
 }
 
 const font = Nunito({ 
@@ -31,7 +31,15 @@ export default async function RootLayout({
   return (
     <html lang="en">
     <head>
-      <link rel="icon" href="/favicon-16x16.png" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content={metadata.description} />
+        <title>{metadata.title}</title>
+        <link rel="icon" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
     </head>
       <body className={font.className}>
         <ClientOnly>
