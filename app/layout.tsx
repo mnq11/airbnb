5315f369b71e460324aca8d48f5097dab21e3344
mnq11 <mnq_77@yahoo.com>
2023-04-30 14:32:31 +0200

    adding titel

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,7 @@ export default async function RootLayout({
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <title></title>
     </head>
       <body className={font.className}>
         <ClientOnly>
